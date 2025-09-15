476dc8adde804f09adb394d35cb6fd4c861fddb3
Naveed123-ux <134216848+Naveed123-ux@users.noreply.github.com>
2025-09-15 17:18:12 +0500

    cfd

--- src/components/LanguageSwitcher.jsx
+++ src/components/LanguageSwitcher.jsx
@@ -103,9 +103,6 @@ const LanguageSwitcher = () => {
       </div>
 
       {/* Optional: Debug info */}
-      <div className="mt-2 text-sm text-gray-500">
-        Current: {currentLanguage}
-      </div>
     </div>
   );
 };
